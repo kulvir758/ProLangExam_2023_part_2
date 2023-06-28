f07ef4f1173972ffa810dee7e79ff0c73996790c
kulvir758 <Skulvir758@gmail.com>
2023-06-28 13:21:23 +0300

    Updated the Repository with all tasks

---
+++ src/app/teacher.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TeacherService {
+
+  constructor() { }
+
+  getTeachers(): Observable<any[]> {
+    const teachers = [
+      {
+        id: 1,
+        firstName: "James",
+        lastName: "Bond",
+        skills: [
+          "Fighting",
+          "Spying",
+          "Weapons",
+          "Driving"
+        ]
+      }, 
+      {
+        id: 2,
+        firstName: "Andrzej",
+        lastName: "Gołota",
+        skills: [
+          "Boxing",
+          "Dancing",
+          "Acting"
+        ]
+      }
+    ];
+
+    return of(teachers);
+  }
+}
